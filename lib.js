f649617c5776a983a3ebff6cf194564866bf17fe
Artur Diniz Adam <artur.daz@gmail.com>
2017-07-08 16:55:26 -0300

    calculaPontos

--- lib.js
+++ lib.js
@@ -2,6 +2,17 @@ function formataMoeda() {
     return "R$ " + valorMoeda.toFixed(2).replace(".", ",")
 }
 
-function pegaNumero(){
-    return parseFloat(prompt(textoPrompt).replace(",", "."))
+function pegaNumero(txt){
+    return parseFloat(prompt(txt).replace(",", "."))
+}
+
+function calculaPontos(nome) {
+    const vitorias = pegaNumero("Digite o número de vitórias do " + nome)
+    const empates = pegaNumero("Digite o número de empates do " + nome)
+    const derrotas= pegaNumero("Digite o número de derrotas do " + nome)
+
+    //variavel local
+    var pontos = vitorias * 2 + empates * 0 + derrotas * -1
+    document.write(nome + " tem " + pontos + " pontos" + "<br>")
+    return pontos
 }
